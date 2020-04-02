c857a75264921114d8484b918ca4712a4222918b
Gustavo Petry <62771548+GustavoPetry@users.noreply.github.com>
2020-04-01 23:13:55 -0300

    Update EX008

--- EX008.py
+++ EX008.py
@@ -7,10 +7,10 @@ while opcao != 3:
     print(" \n 1 - INFORMAR NOTAS \n 2 - EXIBIR AS NOTAS INFORMADAS \n 3 - CALCULAR MÉDIA DAS NOTAS INFORMADAS \n 4 - SAIR DO SISTEMA ")
     opcao = int(input("\n ESCOLHA SUA OPÇÃO: "))
 
-# SE...
+# Se...
     if opcao == 1:
         notas.append(float(input("DIGITE A NOTA: ")))
-# SE...
+# ...
     elif opcao == 2:
         for x in notas:
             print(x)
@@ -18,6 +18,7 @@ while opcao != 3:
         print(sum(notas) / len (notas))
     elif opcao == 4:
         print("\n **** O SISTEMA SENDO ENCERRADO ****")
+#Senão...
     else:
         print("\n **** COMANDO INVÁLIDO, TENTE NOVAMENTE! ****")
 
