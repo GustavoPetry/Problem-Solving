771457640b805dfa1c0ed8655afa21bf9546f8dc
Gustavo Petry <62771548+GustavoPetry@users.noreply.github.com>
2020-04-01 23:03:21 -0300

    Add file via upload

---
+++ EX008.py
@@ -0,0 +1,25 @@
+# Variáveis
+notas = []
+opcao = -1
+
+# Quando a opção for diferente de 3 faça:
+while opcao != 3:
+    print(" \n 1 - INFORMAR NOTAS \n 2 - EXIBIR AS NOTAS INFORMADAS \n 3 - CALCULAR MÉDIA DAS NOTAS INFORMADAS \n 4 - SAIR DO SISTEMA ")
+    opcao = int(input("\n ESCOLHA SUA OPÇÃO: "))
+
+# SE...
+    if opcao == 1:
+        notas.append(float(input("DIGITE A NOTA: ")))
+# SE...
+    elif opcao == 2:
+        for x in notas:
+            print(x)
+    elif opcao == 3:
+        print(sum(notas) / len (notas))
+    elif opcao == 4:
+        print("\n **** O SISTEMA SENDO ENCERRADO ****")
+    else:
+        print("\n **** COMANDO INVÁLIDO, TENTE NOVAMENTE! ****")
+
+
+
