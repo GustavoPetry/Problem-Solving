1b46d2cd9ae1b3c3e916efc9d3cd008b7b01b09d
Gustavo Petry <62771548+GustavoPetry@users.noreply.github.com>
2020-04-10 20:45:05 -0300

    Rename file

---
+++ 012 - Cálculo de Porcentagem.py
@@ -0,0 +1,11 @@
+preco = float(input("Digite o preço incial do produto: "))
+valor_desconto = float(input("Digite o valor de desconto: "))
+desconto = (preco * valor_desconto) / 100
+
+print("O valor do produto digitado de R$ {} com um desconto de {} % fica por: R$ {} ".format(preco, valor_desconto, desconto))
+
+
+
+
+
+
